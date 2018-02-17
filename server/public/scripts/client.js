77dc6a8b493fda923bb598888b27d5e4e56448ca
Selena Carlson <selena.h.carlson@gmail.com>
2018-02-16 19:19:13 -0600

    decent start

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,78 @@
-$(document).ready(onReady());
+$( document ).ready( function(){
+    
+    getTasks();
+    
+    function getTasks(){
+        $.ajax({
+            type: 'GET',
+            url: '/tasks',
+            success: function(response){
+                displayTasks(response);
+            },
+            error: function(response){
+                console.log('error getting tasks')
+            }
+            }); // end ajax get
+    } // end getTasks
 
-function onReady(){
-    console.log('jq');
+    function displayTasks(chores){
+        $('.list').empty();
+        for( chore of chores ){
+        $('.list').append(`<tr><td>${chore.task}</td>
+                <td>${chore.description}</td>
+                <td>${chore.category}</td>
+                <td><button class="complete" data-id=${chore.id}>Done</button></td>
+                <td><button class="delete" data-id=${chore.id}>Delete</button></td>
+                </tr>`)
+        }
+    } // end displayTasks
 
-} //end onReady
+    $( '#addButton' ).on( 'click', function(){
+        let task = $('#taskIn').val();
+        let description = $('#descriptionIn').val();
+        let category = $('#categoryIn').val();
+
+        let newTask = {
+            task: task,
+            description: description,
+            category: category
+        }
+        console.log(newTask);
+        addTask(newTask);
+    });
+
+    function createTask(){
+        let task = $('#taskIn').val();
+        let description = $('#descriptionIn').val();
+        let category = $('#categoryIn').val();
+
+        let newTask = {
+            task: task,
+            description: description,
+            category: category
+        }
+    };
+
+    function addTask(newTask){
+        $.ajax({
+            type: 'POST',
+            url: '/tasks',
+            data: newTask,
+            success: function(){
+                clearInputs();
+                getTasks();
+            },
+            error: function(error){
+                console.log('Adding unsuccessful', newTask);
+                console.log(error);
+            }
+        })
+    } // end addTask
+
+    function clearInputs(){
+        $('#taskIn').val('');
+        $('#descriptionIn').val('');
+        $('#categoryIn').val('');
+    } // end clearInputs
+
+}); // end on ready
--- server/routes/task-router.js
+++ server/routes/task-router.js
@@ -3,4 +3,33 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const pool = require('../modules/pool');
 
+router.get('/', function(request, response){
+    const sqlText = 'SELECT * FROM tasks';
+    pool.query(sqlText)
+        .then(function(result){
+            console.log('Got result:', result)
+            response.send(result.rows);
+        })
+        .catch(function(error){
+            console.log('Error on get', error);
+            response.sendStatus(500);
+        })
+}) // end get
+
+router.post('/', function(request, response){
+    const newTask = request.body;
+    console.log(newTask);
+    const sqlText = `INSERT INTO tasks 
+        (task, description, category) 
+        VALUES ($1, $2, $3)`;
+    pool.query(sqlText, [newTask.task, newTask.description, newTask.category])
+        .then(function(result){
+            response.sendStatus(201);
+        })
+        .catch(function(error){
+            response.sendStatus(500)
+            console.log(newTask);
+        })
+}) // end post
+
 module.exports = router;
